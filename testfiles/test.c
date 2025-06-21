7db6631c07c79c19358adec28192fc1ae265e888
Zhang Ze <zj1491167926@163.com>
2025-06-21 16:44:48 +0800

    test-new

--- testfiles/test.c
+++ testfiles/test.c
@@ -5,5 +5,7 @@ int main(void){
     s = 'a'; // Assigning a character to variable s
     printf("The character is: %c\n", s); // Printing the character stored in s
     printf("The size of s is: %zu bytes\n", sizeof(s)); // Printing the size of variable s
+    printf("The size of int is: %zu bytes\n", sizeof(int)); // Printing the size of int type
+    printf("The size of char is: %zu bytes\n", sizeof(char)); // Printing the size of char type
     return 0; 
 }
